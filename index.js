ffd4b7a757e91b08ca95a2801b1da48028480faa
nhcarrigan <nhcarrigan@gmail.com>
2021-01-14 14:44:50 -0800

    github -> chat
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ client.on("ready", () => {
     pid: process.pid,
     activity: {
       assets: {
-        large_image: 'main',
+        large_image: "main",
         large_text: "It's Becca!",
       },
       details: "Check out what I'm doing!",
       buttons: [
         { label: "Website", url: "https://www.nhcarrigan.com" },
         {
-          label: "GitHub",
-          url: "https://github.com/nhcarrigan",
+          label: "Chat",
+          url: "http://chat.nhcarrigan.com",
         },
       ],
     },
